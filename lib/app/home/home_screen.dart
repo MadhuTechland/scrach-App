576fe18691ffc5d0fe4652079ac917069c8d5898
Subramanyambandi <subramanyambandi8@gmail.com>
2025-04-03 15:13:11 +0530

    added carousel images

--- lib/app/home/home_screen.dart
+++ lib/app/home/home_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scratch_app/app/home/notification_screen.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0; // Tracks current page index
+  final List<String> images = [
+    'assets/images/image.png',
+    'assets/images/image.png',
+    'assets/images/image.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +28,7 @@ class HomeScreen extends StatelessWidget {
           Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
-                colors: [Colors.white,Colors.red, Colors.yellowAccent],
+                colors: [Colors.white, Colors.red, Colors.yellowAccent],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomCenter,
               ),
@@ -27,12 +41,12 @@ class HomeScreen extends StatelessWidget {
               // App Bar Section
               Container(
                 height: MediaQuery.of(context).size.height * 0.18,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24, vertical: 20), 
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
                 decoration: const BoxDecoration(
-                  color: Colors.red, 
+                  color: Colors.red,
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30), 
+                    bottomLeft: Radius.circular(30),
                     bottomRight: Radius.circular(30),
                   ),
                 ),
@@ -47,17 +61,14 @@ class HomeScreen extends StatelessWidget {
                           'assets/images/logo.png',
                           height: 60,
                         ),
-
-                        
-
                         InkWell(
                           onTap: () {
-                                Get.to(()=>const NotificationListScreen());
+                            Get.to(() => const NotificationListScreen());
                           },
                           child: Center(
                             child: Image.asset(
                               'assets/images/notification_image.png',
-                              height: 40, 
+                              height: 40,
                             ),
                           ),
                         ),
@@ -67,9 +78,53 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
 
+              const SizedBox(height: 10),
+
+              CarouselSlider(
+                items: images.map((image) {
+                  return ClipRRect(
+                    // Apply ClipRRect inside CarouselSlider
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      image,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 200,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                ),
+              ),
+
+              // Dots Indicator
+              const SizedBox(height: 10),
+              AnimatedSmoothIndicator(
+                activeIndex: _currentIndex,
+                count: images.length,
+                effect: const WormEffect(
+                  dotWidth: 10,
+                  dotHeight: 10,
+                  activeDotColor: Colors.white, // Change color as needed
+                  dotColor: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+
               // Image List Section
               Expanded(
                 child: ListView.builder(
+                  // shrinkWrap: true,
+                  // physics: const NeverScrollableScrollPhysics(),
                   itemCount: 8,
                   itemBuilder: (context, index) {
                     return Padding(
